8f4cda4d390896a093651314bd646b94ffb11da1
Waseem Khan <waseem.wk22@yahoo.com>
2025-02-19 16:37:02 +0100

    Add files via upload

---
+++ wkLibrary/Tests/wkLibraryTests/wkLibraryTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import wkLibrary
+
+final class wkLibraryTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
